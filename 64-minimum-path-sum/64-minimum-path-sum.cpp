85233270c6c77de7f83da623344e0c934e5977e1
Sahil Adhav <sahiladhav16@gmail.com>
2022-08-26 10:27:20 +0530

    Time: 17 ms (43.26%), Space: 10 MB (65.36%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -21,36 +21,71 @@ public:
 //     }
     
     int minPathSum(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+//         for(int i=0; i<m; i++){
+//             for(int j=0; j<n; j++){
+//                 if(i==0 and j==0){
+//                     dp[i][j] = grid[i][j];
+//                 }
+//                 else{
+//                     int left = grid[i][j];
+//                     int up = grid[i][j];
+                    
+//                     if(j > 0){
+//                         left += dp[i][j-1];
+//                     }
+//                     else{
+//                         left += 1e9;
+//                     }
+                    
+//                     if(i > 0){
+//                         up += dp[i-1][j];
+//                     }
+//                     else{
+//                         up += 1e9;
+//                     }
+//                     dp[i][j] = min(left, up);
+//                 }
+//             }
+//         }
+//         return dp[m-1][n-1];
+        
+        
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> prev(n, 0);
         
         for(int i=0; i<m; i++){
+            vector<int> curr(n, 0);
             for(int j=0; j<n; j++){
                 if(i==0 and j==0){
-                    dp[i][j] = grid[i][j];
+                    curr[j] = grid[i][j];
                 }
                 else{
                     int left = grid[i][j];
                     int up = grid[i][j];
                     
                     if(j > 0){
-                        left += dp[i][j-1];
+                        left += curr[j-1];
                     }
                     else{
                         left += 1e9;
                     }
                     
                     if(i > 0){
-                        up += dp[i-1][j];
+                        up += prev[j];
                     }
                     else{
                         up += 1e9;
                     }
-                    dp[i][j] = min(left, up);
+                    curr[j] = min(left, up);
                 }
             }
+            prev = curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
