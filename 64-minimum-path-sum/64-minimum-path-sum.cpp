39e6f91e288a2322022d672d5234806988dfce13
Sahil Adhav <sahiladhav16@gmail.com>
2022-08-26 10:24:39 +0530

    Time: 11 ms (81.22%), Space: 10.2 MB (6.30%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,29 +1,56 @@
 class Solution {
 public:
     
-    int f(vector<vector<int>>& grid, vector<vector<int>>& dp, int i, int j){
-        if(i == 0 and j == 0){
-            return grid[i][j];
-        }
+//     int f(vector<vector<int>>& grid, vector<vector<int>>& dp, int i, int j){
+//         if(i == 0 and j == 0){
+//             return grid[i][j];
+//         }
         
-        if(i < 0 or j < 0){
-            return 1e9;
-        }
+//         if(i < 0 or j < 0){
+//             return 1e9;
+//         }
         
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         }
         
-        int left = grid[i][j] + f(grid, dp, i, j-1);
-        int up = grid[i][j] + f(grid, dp, i-1, j);
+//         int left = grid[i][j] + f(grid, dp, i, j-1);
+//         int up = grid[i][j] + f(grid, dp, i-1, j);
         
-        return dp[i][j] = min(left, up);
-    }
+//         return dp[i][j] = min(left, up);
+//     }
     
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return f(grid, dp, m-1, n-1);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 and j==0){
+                    dp[i][j] = grid[i][j];
+                }
+                else{
+                    int left = grid[i][j];
+                    int up = grid[i][j];
+                    
+                    if(j > 0){
+                        left += dp[i][j-1];
+                    }
+                    else{
+                        left += 1e9;
+                    }
+                    
+                    if(i > 0){
+                        up += dp[i-1][j];
+                    }
+                    else{
+                        up += 1e9;
+                    }
+                    dp[i][j] = min(left, up);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
