9825df0e0025a6bda291fa367583c7b3de2000b1
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-04 00:25:27 +0530

    Time: 75 ms (23.99%), Space: 18.7 MB (29.27%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -22,7 +22,23 @@ int f(int ind, vector<int>& a, int T, vector<vector<int>> &dp){
     
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
-        return f(n-1, coins, amount, dp);
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, 0));
+        
+        for(int T=0; T<=amount; T++){
+            dp[0][T] = (T % coins[0] == 0);
+        }
+        
+        for(int ind=1; ind<n; ind++){
+            for(int T=0; T<=amount; T++){
+                int notPick = dp[ind-1][T];
+                int pick = 0;
+                if(coins[ind] <= T){
+                    pick = dp[ind][T-coins[ind]];
+                }
+                dp[ind][T] = pick + notPick;
+            }
+        }
+        
+        return dp[n-1][amount];
     }
 };
