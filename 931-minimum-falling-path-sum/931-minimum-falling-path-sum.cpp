53a6ef6c5b5a6fc537b3f40e6f492586f081227f
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-04 22:49:25 +0530

    Time: 22 ms (58.90%), Space: 10.4 MB (26.42%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -19,11 +19,28 @@ public:
     
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int r = matrix.size(), c = matrix[0].size();
-        vector<vector<int>> dp(r+1, vector<int>(c+1, -1));
+        vector<vector<int>> dp(r+1, vector<int>(c+1, 0));
+        
+        for(int j=0; j<c; j++) dp[0][j] = matrix[0][j];
+        
+        for(int i=1; i<r; i++){
+            for(int j=0; j<c; j++){
+                int up = matrix[i][j] + dp[i-1][j];
+                int left = matrix[i][j];
+                if(j-1 >= 0) left += dp[i-1][j-1];
+                else left += 1e9;
+                int right = matrix[i][j];
+                if(j+1 < c) right += dp[i-1][j+1];
+                else right += 1e9;
+                dp[i][j] = min({up, left, right});
+            }
+        }
+        
+        
         int maxi = INT_MAX;
         int n = matrix.size();
-        for(int i=0; i<matrix.size(); i++){
-            maxi = min(maxi, f(n-1, i, dp, matrix));
+        for(int j=0; j<matrix.size(); j++){
+            maxi = min(maxi, dp[r-1][j]);
         }
         return maxi;
     }
