86181259a80bb5a9dfb4d0e929ee7d9b66c30ae6
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-09 00:33:57 +0530

    Time: 50 ms (24.85%), Space: 13 MB (37.55%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,87 +1,53 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string str1, string str2) {
-        int m = str1.length(), n = str2.length();
-        int t[m+1][n+1];
+    string shortestCommonSupersequence(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
         
-        // string for storing shortest common super subsequence
-        string ind = "";
+        for(int i=0; i<=n; i++) dp[i][0] = 0;
+        for(int j=0; j<=m; j++) dp[0][j] = 0;
         
-        // filling dp / t Matrix
-        
-        for(int i = 0;i < m+1; ++i)
-        {
-            for(int j = 0; j< n+1; ++j)
-            {
-                // Initialising first row and first column of dp / t matrix to 0
-                if(i == 0 || j == 0)
-                    t[i][j] = 0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
         
-        for(int i = 1; i < m+1; ++i)
-        {
-            for(int j = 1; j < n+1; ++j)
-            {
-                if(str1[i-1] == str2[j-1])
-                    t[i][j] = 1 + t[i-1][j-1];
-                else
-                    t[i][j] = max(t[i-1][j], t[i][j-1]);
+        string ans = "";
+        int i=n, j=m;
+        
+        while(i > 0 && j > 0){
+            if(s[i-1] == t[j-1]){
+                ans += s[i-1];
+                i--, j--;
+            }
+            else if(dp[i-1][j] > dp[i][j-1]){
+                ans += s[i-1];
+                i--;
+            }
+            else{
+                ans += t[j-1];
+                j--;
             }
         }
         
-        // making shortest common supersubsequence
-        int i = m, j = n;
-        
-        while(i > 0 && j > 0)
-        {
-            if(str1[i-1] == str2[j-1])
-            {
-                // if current character in str1 and str2 are same, then
-                // current character is part of LCS
-                
-                ind.push_back(str1[i-1]); // put current character of LCS in ind
-                --i;                     // reduce values of i,j
-                --j;
-            }
-            
-            // if current character in str1 and str2 are different
-            
-            else if(t[i][j-1] > t[i-1][j])
-            {
-                ind.push_back(str2[j-1]);  // put current character of str2 in ind
-                --j;                       // reduce value of j
-            }
-            
-            else
-            {
-                ind.push_back(str1[i-1]);  // put current character of str1 in ind
-                --i;                       // reduce value of i 
-            }
+        while(i > 0){
+            ans += s[i-1];
+            i--;
         }
         
-        // if str2 reaches its end,  put remaining characters of
-        // str1 in ind
-        
-        while( i > 0)
-        {
-            ind.push_back(str1[i-1]);
-            --i;
+        while(j > 0){
+            ans += t[j-1];
+            j--;
         }
         
-        // if str1 reaches its end, put remaining characters of 
-        // str2 in ind
-        
-        while( j > 0)
-        {
-            ind.push_back(str2[j-1]);
-            --j;
-        }
-        
-        // reverse LCS as we started storing from t[m][n]
-        reverse(ind.begin(),ind.end());
-        
-        // return the LCS
-        return ind;
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
