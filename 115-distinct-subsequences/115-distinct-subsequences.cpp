01a338220e6f28374a430151a6f3601f86b98dbe
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-10 12:53:59 +0530

    Time: 132 ms (5.94%), Space: 30.1 MB (16.98%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -3,15 +3,15 @@ public:
     
     int f(string s, string t, int i, int j, vector<vector<int>> &dp){
         //Base Case
-        if(j < 0) return 1;
-        if(i < 0) return 0;
+        if(j == 0) return 1;
+        if(i == 0) return 0;
         
         if(dp[i][j] != -1){
             return dp[i][j];
         }
         
         //Calculation
-        if(s[i] == t[j]){
+        if(s[i-1] == t[j-1]){
             return dp[i][j] = f(s, t, i-1, j-1, dp) + f(s, t, i-1, j, dp);
         }
         return dp[i][j] = f(s, t, i-1, j, dp);
@@ -20,7 +20,28 @@ public:
     int numDistinct(string s, string t) {
         int n = s.length();
         int m = t.length();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return f(s, t, n-1, m-1, dp);
+        vector<vector<double>> dp(n+1, vector<double>(m+1, 0));
+        
+        //Base case
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int j=1; j<=m; j++){
+            dp[0][j] = 0;
+        }
+        
+        //calculation
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return (int)dp[n][m];
     }
 };
