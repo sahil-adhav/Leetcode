775793a0751f1086ae193a61983a3e4968911d9e
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-10 13:09:29 +0530

    Time: 48 ms (69.94%), Space: 6.4 MB (89.47%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -26,15 +26,15 @@ public:
         
         //calculation
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
+            for(int j=m; j>=1; j--){
                 if(s[i-1] == t[j-1]){
-                    curr[j] = prev[j-1] + prev[j];
+                    prev[j] = prev[j-1] + prev[j];
                 }
                 else{
-                    curr[j] = prev[j];
+                    prev[j] = prev[j];
                 }
             }
-            prev = curr;
+            // prev = curr;
         }
         return (int)prev[m];
     }
