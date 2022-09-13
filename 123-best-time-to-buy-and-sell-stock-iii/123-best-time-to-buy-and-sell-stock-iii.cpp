129e95e2a20fb3beb3d2834a7ca0faa4a008cc3d
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-13 19:52:30 +0530

    Time: 1498 ms (10.80%), Space: 207.1 MB (30.55%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -33,15 +33,16 @@ public:
         for(int day=n-1; day>=0; day--){
             for(int buy=0; buy<=1; buy++){
                 for(int cap=1; cap<=2; cap++){
+                    int profit = INT_MIN;
                     if(buy==1){
-                        dp[day][buy][cap] = max(-prices[day] + dp[day+1][0][cap], 
+                        profit = max(-prices[day] + dp[day+1][0][cap], 
                                      0 + dp[day+1][1][cap]);
                     }
                     else{
-                        dp[day][buy][cap] = max(prices[day] + dp[day+1][1][cap-1], 
+                        profit = max(prices[day] + dp[day+1][1][cap-1], 
                                     0 + dp[day+1][0][cap]);
                     }
-                    // dp[day][buy][cap] = profit;
+                    dp[day][buy][cap] = profit;
                 }
             }
         }
