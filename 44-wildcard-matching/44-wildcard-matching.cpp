cd518746664e56adca9117063c258b313c057c9f
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-13 16:12:44 +0530

    Time: 146 ms (26.10%), Space: 11.2 MB (71.10%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-        int n = s.length();
-        int m = p.length();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return f(n, m, s, p, dp);
-    }
     
-    bool f(int i, int j, string &s, string &p, vector<vector<int>> &dp){
+        bool f(int i, int j, string &s, string &p, vector<vector<int>> &dp){
         //Base Case
         if(i==0 and j==0) return true;
         
@@ -41,6 +35,51 @@ public:
         }
         
         return dp[i][j] = false;
+    }
+    
+    bool isMatch(string s, string p) {
+        int n = s.length();
+        int m = p.length();
+        vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
+        
+        //Base Cases
+        dp[0][0] = true;
+        
+        for(int j=1; j<=m; j++){
+            bool flag = true;
+            for(int ii=1; ii<=j; ii++){
+                if(p[ii-1] != '*'){
+                    flag = false;
+                }
+            }
+            dp[0][j] = flag;
+        }
+        
+        for(int i=1; i<=n; i++){
+            bool flag = true;
+            for(int ii=1; ii<=i; ii++){
+                if(s[ii-1] != '*'){
+                    flag = false;
+                    break;
+                }
+            }
+            dp[i][0] = flag;
+        }
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s[i-1] == p[j-1] or s[i-1] == '?' or p[j-1] == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+
+                else if(s[i-1] == '*' or p[j-1] == '*'){
+                    dp[i][j] = dp[i-1][j] | dp[i][j-1];
+                }
+
+                else dp[i][j] = false;
+            }
+        }
+        return dp[n][m];
     }
 };
 
