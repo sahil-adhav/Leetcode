e71685bb578c7ef748ac04445664a8f617ed9b5a
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-13 21:20:48 +0530

    Time: 38 ms (26.41%), Space: 14.5 MB (10.76%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -25,7 +25,26 @@ public:
         int n = prices.size();
         //days ---- buy/sell ---- cap
         // days x 2 x k
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, -1)));
-        return f(0,1,k,n,prices, dp);
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+        
+        //base cases
+        for(int day=n-1; day>=0; day--){
+            for(int buy=0; buy<=1; buy++){
+                for(int cap=1; cap<=k; cap++){
+                    int maxProfit = INT_MIN;
+                    if(buy == 1){
+                        maxProfit = max(-prices[day] + dp[day+1][0][cap], 
+                                    0 + dp[day+1][1][cap]);
+                    }
+                    else{
+                        maxProfit = max(prices[day] + dp[day+1][1][cap-1], 
+                                    0 + dp[day+1][0][cap]);    
+                    }
+
+                    dp[day][buy][cap] = maxProfit;                    
+                }
+            }
+        }
+        return dp[0][1][k];
     }
 };
