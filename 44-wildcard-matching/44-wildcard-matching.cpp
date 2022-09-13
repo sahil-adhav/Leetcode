55505b3fe508702956d3953de2bb21a4b83cd935
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-13 16:01:04 +0530

    Time: 95 ms (51.33%), Space: 27.9 MB (15.41%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -3,26 +3,26 @@ public:
     bool isMatch(string s, string p) {
         int n = s.length();
         int m = p.length();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(n-1, m-1, s, p, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return f(n, m, s, p, dp);
     }
     
     bool f(int i, int j, string &s, string &p, vector<vector<int>> &dp){
         //Base Case
-        if(i<0 and j<0) return true;
+        if(i==0 and j==0) return true;
         
-        if(i<0 and j>=0){
-            for(int ii=0; ii<=j; ii++){
-                if(p[ii] != '*'){
+        if(i==0 and j>0){
+            for(int ii=1; ii<=j; ii++){
+                if(p[ii-1] != '*'){
                     return false;
                 }
             }    
             return true;
         }
         
-        if(i>=0 and j<0){
-            for(int ii=0; ii<=i; ii++){
-                if(s[ii] != '*'){
+        if(i>0 and j==0){
+            for(int ii=1; ii<=i; ii++){
+                if(s[ii-1] != '*'){
                     return false;
                 }
             }
@@ -32,11 +32,11 @@ public:
         if(dp[i][j] != -1) return dp[i][j];
         
         //Calculation Part
-        if(s[i] == p[j] or s[i] == '?' or p[j] == '?'){
+        if(s[i-1] == p[j-1] or s[i-1] == '?' or p[j-1] == '?'){
             return dp[i][j] = f(i-1, j-1, s, p, dp);
         }
         
-        if(s[i] == '*' or p[j] == '*'){
+        if(s[i-1] == '*' or p[j-1] == '*'){
             return dp[i][j] = f(i-1, j, s, p, dp) || f(i, j-1, s, p, dp);
         }
         
