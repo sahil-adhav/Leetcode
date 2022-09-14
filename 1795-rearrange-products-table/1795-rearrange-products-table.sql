667982725185abcb7d7910e6ce5ee5865eba0733
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-14 16:58:13 +0530

    Time: 1023 ms (13.17%), Space: 0B (100.00%) - LeetHub

---
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -0,0 +1,7 @@
+select * from (
+select product_id,'store1' as store, store1 as price from Products
+union 
+select product_id,'store2' as store, store2 as price from Products
+union
+select product_id,'store3' as store, store3 as price from Products
+) store where price is not null
