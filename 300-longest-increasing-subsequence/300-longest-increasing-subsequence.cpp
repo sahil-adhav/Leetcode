435385c4583ed1ce36cc2013db25b9552496e009
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-16 11:48:44 +0530

    Time: 11 ms (88.71%), Space: 10.3 MB (87.91%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
-    
-    int f(int ind, int prev, vector<vector<int>> &dp, int n, vector<int> &nums){
-        if(ind == n) return 0;
-        if(dp[ind][prev+1] != -1) return dp[ind][prev+1];
-        
-        int notPick = f(ind+1, prev, dp, n, nums);
-        int pick = -1e9;
-        if(prev == -1 or nums[ind] > nums[prev]) pick = 1 + f(ind+1, ind, dp, n, nums);
-        
-        return dp[ind][prev+1] = max(pick, notPick);
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
         
-        for(int i=n-1; i>=0; i--){
-            for(int prev=i-1; prev>=-1; prev--){
-                int notPick = dp[i+1][prev+1];
-                int pick = -1e9;
-                if(prev == -1 or nums[i] > nums[prev]){
-                    pick = 1 + dp[i+1][i+1];
-                }
-
-                dp[i][prev+1] = max(pick, notPick);
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        
+        for(int i=0; i<n; i++){
+            if(nums[i] > temp.back()){
+                temp.push_back(nums[i]);
+            }
+            else{
+                int ind = lower_bound(temp.begin(), temp.end(), nums[i]) 
+                    - temp.begin();
+                temp[ind] = nums[i];
             }
         }
-        return dp[0][-1+1];
+        return temp.size();
     }
 };
+
+// Input: nums = [10,9,2,5,3,7,101,18]
+// Output: 4
+
+// => 2 3 7 101  
+    
+// Binary Search
+
+// 3 5 18 101 
+
+
+// 10 101
+
+// 9 101
+    
+// 2 5 7 101
+    
+// 3 7 101
