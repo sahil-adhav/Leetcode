161de53c49254ce1f0b9cbe4b0db2c18d3d28efc
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-16 13:45:43 +0530

    Time: 307 ms (59.39%), Space: 10.7 MB (21.50%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,17 +2,39 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, 1);
+        vector<int> dp(n, 1), hash(n);
         int maxi = 1;
-        
+        int lastIdx = 0;
+
         for(int curr=0; curr<n; curr++){
+            hash[curr] = curr;
             for(int prevInd=0; prevInd<curr; prevInd++){
-                if(nums[prevInd] < nums[curr]){
-                    dp[curr] = max(1 + dp[prevInd], dp[curr]);
+                if(nums[prevInd] < nums[curr] && 1+dp[prevInd] > dp[curr]){
+                    dp[curr] = 1 + dp[prevInd];
+                    hash[curr] = prevInd;
                 }
             }
-            maxi = max(maxi, dp[curr]);
+            if(dp[curr] > maxi){
+                maxi = dp[curr];
+                lastIdx = curr;
+            }
         }
+        
+        vector<int> list;
+        list.push_back(nums[lastIdx]);
+        
+        while(hash[lastIdx] != lastIdx){
+            lastIdx = hash[lastIdx];
+            list.push_back(nums[lastIdx]);
+        }
+        
+        reverse(list.begin(), list.end());
+        
+        for(auto it : list){
+            cout << it << " ";
+        }
+        cout << endl;
+        
         return maxi;
     }
 };
