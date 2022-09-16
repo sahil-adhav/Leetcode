78c45067f3385441897097ac897cdf983a077d94
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-16 12:52:56 +0530

    Time: 323 ms (57.87%), Space: 10.6 MB (25.34%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,39 +2,23 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        
-        vector<int> temp;
-        temp.push_back(nums[0]);
-        int len = 1;
-        for(int i=1; i<n; i++){
-            if(nums[i] > temp.back()){
-                temp.push_back(nums[i]);
-                len++;
+        vector<int> dp(n, 1);
+        int maxi  = 1;        
+        for(int curr=0; curr<n; curr++){
+            for(int prev=0; prev<curr; prev++){
+                if(nums[curr] > nums[prev]){
+                    dp[curr] = max(dp[curr], 1+dp[prev]);
+                }
             }
-            else{
-                int ind = lower_bound(temp.begin(), temp.end(), nums[i]) 
-                    - temp.begin();
-                temp[ind] = nums[i];
-            }
+            maxi = max(maxi, dp[curr]);
         }
-        return len;
+        return maxi;
     }
 };
 
+
 // Input: nums = [10,9,2,5,3,7,101,18]
+//      :   dp = [1, 1,1,2,2,3, 4 , 4]   
+    
+
 // Output: 4
-
-// => 2 3 7 101  
-    
-// Binary Search
-
-// 3 5 18 101 
-
-
-// 10 101
-
-// 9 101
-    
-// 2 5 7 101
-    
-// 3 7 101
