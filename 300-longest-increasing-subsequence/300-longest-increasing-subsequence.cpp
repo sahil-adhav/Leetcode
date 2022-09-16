3e4d148816e1573bac57372cc4963b9531faec1f
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-16 11:17:46 +0530

    Time: 9 ms (92.50%), Space: 10.4 MB (87.91%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    
-    int f(int ind, int prev, int n, vector<int> &nums, vector<vector<int>> &dp){
-        if(ind == n) return 0;
-        if(dp[ind][prev+1] != -1) return dp[ind][prev+1];
-        int notPick = f(ind+1, prev, n, nums, dp);
-        int pick = -1e9;
-        if(prev == -1 or nums[ind] >  nums[prev]) pick = 1 + f(ind+1, ind, n, nums, dp);
-        
-        return dp[ind][prev+1] = max(pick, notPick);
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ahead(n+1, 0), curr(n+1, 0);
         
-        for(int ind=n-1; ind>=0; ind--){
-            for(int prev = ind-1; prev>=-1; prev--){
-                int notPick = ahead[prev+1];
-                int pick = -1e9;
-                if(prev == -1 or nums[ind] >  nums[prev]){ 
-                    pick = 1 + ahead[ind+1];
-                }
-
-                curr[prev+1] = max(pick, notPick);
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        
+        for(int i=0; i<n; i++){
+            if(nums[i] > temp.back()){
+                temp.push_back(nums[i]);
             }
-            ahead = curr;
+            else{
+                int ind = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+                temp[ind] = nums[i];
+            }
         }
-        return ahead[-1+1];
+        return temp.size();
     }
 };
