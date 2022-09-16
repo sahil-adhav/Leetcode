ae99cb469433b78729c11e784594712b4d52d204
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-16 13:40:44 +0530

    Time: 649 ms (21.38%), Space: 10.5 MB (67.09%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,44 +2,17 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, 1), hash(n, 1);
-        int lastIndex = -1;
-        int maxi  = -1;        
+        vector<int> dp(n, 1);
+        int maxi = 1;
+        
         for(int curr=0; curr<n; curr++){
-            hash[curr] = curr;
-            for(int prev=0; prev<curr; prev++){
-                if(nums[prev] < nums[curr] && 1+dp[prev] > dp[curr]){
-                    dp[curr] = 1 + dp[prev];
-                    hash[curr] = prev;
+            for(int prevInd=0; prevInd<curr; prevInd++){
+                if(nums[prevInd] < nums[curr]){
+                    dp[curr] = max(1 + dp[prevInd], dp[curr]);
                 }
             }
-            // maxi = max(maxi, dp[curr]);
-  
+            maxi = max(maxi, dp[curr]);
         }
-        
-        for(int curr=0; curr<n; curr++){
-           if(dp[curr] > maxi){
-                maxi = dp[curr];
-                lastIndex = curr;
-            }
-        }
-        
-        vector<int> lis;
-        lis.push_back(nums[lastIndex]);
-        while(hash[lastIndex] != lastIndex){
-            lastIndex = hash[lastIndex];
-            lis.push_back(nums[lastIndex]);
-        }
-        reverse(lis.begin(), lis.end());
-        for(auto it : nums) cout << it << " ";
-        cout << endl;
         return maxi;
     }
 };
-
-
-// Input: nums = [10,9,2,5,3,7,101,18]
-//      :   dp = [1, 1,1,2,2,3, 4 , 4]
-    
-
-// Output: 4
