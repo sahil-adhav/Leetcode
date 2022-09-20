9d5a58f8f9724c3c573ef591821ad44af0e650d0
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-20 15:22:24 +0530

    Time: 693 ms (50.95%), Space: 10.3 MB (73.50%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -20,7 +20,20 @@ public:
         int n = nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        return f(1, n, dp, nums);
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+        
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=n; j++){
+                if(i > j) continue;
+                int maxi = -1e9;
+                for(int k=i; k<=j; k++){
+                    int cost = nums[i-1] * nums[k] * nums[j+1] + 
+                        dp[i][k-1] + dp[k+1][j];
+                    maxi = max(maxi, cost);
+                }
+                dp[i][j] = maxi;  
+            }
+        }
+        return dp[1][n];
     }
 };
