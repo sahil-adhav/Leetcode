3467a4d227f4ff9379ab6386c816095afd321cf9
Sahil Adhav <sahiladhav16@gmail.com>
2022-09-25 20:50:12 +0530

    Time: 10 ms (75.19%), Space: 42.3 MB (34.14%) - LeetHub

---
+++ 1116-print-zero-even-odd/1116-print-zero-even-odd.java
@@ -0,0 +1,40 @@
+class ZeroEvenOdd {
+    private int n;
+    Semaphore s0, sEven, sOdd;
+    
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+        s0 = new Semaphore(1);
+        sEven = new Semaphore(0);
+        sOdd = new Semaphore(0);
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= this.n; i++) {
+            s0.acquire();
+            printNumber.accept(0);
+            if (i % 2 == 0) {
+                sEven.release();
+            }else {
+                sOdd.release();
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= this.n; i += 2) {
+            sEven.acquire();
+            printNumber.accept(i);
+            s0.release();
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= this.n; i += 2) {
+            sOdd.acquire();
+            printNumber.accept(i);
+            s0.release();
+        }
+    }
+}
