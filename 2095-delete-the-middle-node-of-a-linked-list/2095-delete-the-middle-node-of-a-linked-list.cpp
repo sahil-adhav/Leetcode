7fbc3334288b805064fe110fc7d9875f7b295af2
Sahil Adhav <sahiladhav16@gmail.com>
2022-10-14 21:27:55 +0530

    Time: 2160 ms (29.15%), Space: 294.9 MB (20.46%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -11,14 +11,24 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        if (head->next == nullptr)
-        return nullptr;
-    auto slow = head, fast = head->next->next;
-    while (fast != nullptr && fast->next != nullptr) {
-        fast = fast->next->next;
-        slow = slow->next;
-    }
-    slow->next = slow->next->next;
-    return head;
+       if (head == NULL) {
+          return NULL;
+       }
+       if (head->next == NULL) {
+          delete head;
+          return NULL;
+       }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = head; // keeping this prev node to get the previous node of slow pointer when we reach middle
+        while(fast != NULL && fast->next != NULL)
+        {
+            if(slow != head) prev = prev->next; // since we want prev to start one step later than slow node
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        prev->next = slow->next;
+        delete slow;
+        return head;
     }
 };
