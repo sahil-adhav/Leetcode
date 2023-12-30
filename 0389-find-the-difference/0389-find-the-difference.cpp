618ef7b84ed962261707bf58e86f7301a3b265a8
Sahil Adhav <sahiladhav16@gmail.com>
2023-12-30 19:59:41 +0530

    Time: 8 ms (7.05%), Space: 7.2 MB (29.04%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+         unordered_map<char,int> mp;
+        for(auto& c:s) mp[c]+=1;
+        for(auto& c:t) if(--mp[c]<0) return c;
+        return '\0';
+    }
+};
