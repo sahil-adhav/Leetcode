46fdb3de9673ae6c59c2cd9e7bc9fa3a83bd1d2d
Sahil Adhav <sahiladhav16@gmail.com>
2024-01-03 21:26:13 +0530

    Time: 87 ms (69.45%), Space: 23.4 MB (71.52%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
+template <typename InputIt, typename T>
+int countOwn(InputIt first, InputIt last, const T& value) {
+    int count = 0;
+    for (InputIt it = first; it != last; ++it) {
+        if (*it == value) {
+            ++count;
+        }
+    }
+    return count;
+}
+    
     int numberOfBeams(vector<string>& bank) {
-        int pre=0,cur=0,res=0;
-        for(int i=0;i<bank.size();i++){
-            cur=count(bank[i].begin(),bank[i].end(),'1');
-            if(cur>0){
-                res+=pre*cur;
-                pre=cur;
+        int len = bank.size();
+        int previous = 0, current = 0, totalLines = 0;
+        
+        for(int i=0; i<len; i++){
+            current = countOwn(bank[i].begin(), bank[i].end(), '1');
+            if(current > 0){
+                totalLines += current * previous;
+                previous = current;
             }
         }
-        return res;
-
+        return totalLines;
     }
 };
